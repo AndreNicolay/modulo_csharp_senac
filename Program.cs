870b889414a05f8a30e2f481f903240d25d8050e
André Nicolay <andreaugustonicolaynicolay@gmail.com>
2026-07-15 19:47:39 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class Aula
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite seu nome:");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite sua idade:");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (idade <= 20)
+        {
+            Console.WriteLine($"Olá {nome}, você é novo.");
+        } else if (idade <= 35)
+        {
+            Console.WriteLine($"Olá {nome}, você é jovem.");
+        } else
+        {
+            Console.WriteLine($"Olá {nome}, você é velho.");
+        }
+
+        Console.WriteLine($"Olá {nome}!");
+        Console.WriteLine($"Você tem {idade} anos.");
+    }
+}
