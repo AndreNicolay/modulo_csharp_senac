c2269b2e02331a3a76d17bb35e2d7a6a592b4190
André Nicolay <andreaugustonicolaynicolay@gmail.com>
2026-07-16 20:53:49 -0300

    Projeto area retangulo

---
+++ Aula3-area-retangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class AreaRetangulo
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite a base: ");
+        string entradaBase = Console.ReadLine();
+        double baseRetangulo = Convert.ToDouble(entradaBase);
+
+        Console.Write("Digite a altura: ");
+        string entradaAltura = Console.ReadLine();
+        double altura = Convert.ToDouble(entradaAltura);
+
+        double area = baseRetangulo * altura;
+
+        Console.WriteLine();
+        Console.WriteLine($"Área do retângulo: {baseRetangulo} x {altura} = {area}");
+    }
+}
